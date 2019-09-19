87c74257f42e530d81ff0b5f47ed4d517f860880
daschne8 <daschne8@gmail.com>
2019-09-19 15:58:34 -0400

    added router

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  
+
   namespace :api do
     namespace :v1 do
       get :projects, to: 'projects#get_projects'
       get :emails, to: 'emails#send'
     end
+  end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
   end
 
 
